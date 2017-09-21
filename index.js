a6257f2b2f51462f6e84f7cea19b9364216b3bd2
Dragos <dragosgn@gmail.com>
2017-09-21 10:53:16 +0200

    created network and training

--- index.js
+++ index.js
@@ -10,3 +10,29 @@ const testSet = set.test
 
 
 // Creating neuron network
+const Layer = synaptic.Layer
+const Network = synaptic.Network
+const Trainer = synaptic.Trainer
+
+const inputLayer = new Layer(784)
+const hiddenLayer = new Layer(100)
+const outputLayer = new Layer(10)
+
+inputLayer.project(hiddenLayer)
+hiddenLayer.project(outputLayer)
+
+const myNetwork = new Network({
+  input: inputLayer,
+  hidden: [hiddenLayer],
+  output: outputLayer
+})
+
+const traineer = new Trainer(myNetwork)
+trainer.train(trainingSet, {
+  rate: .2,
+  iterations: 20,
+  error: .1,
+  shuffle: true,
+  log: 1,
+  const: Trainer.const.CROSS_ENTROPY
+})
